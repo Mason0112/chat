24181fb75981a1324facddef479e3b02e11bb33b
DESKTOP-NELQMJ0\Mason <willy55406@gmail.com>
2024-10-08 11:43:28 +0800

    註解加註版本1

--- src/main/java/com/mason/chat/chat/ChatController.java
+++ src/main/java/com/mason/chat/chat/ChatController.java
@@ -5,19 +5,16 @@ import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.stereotype.Controller;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 @Controller
 public class ChatController {
 
-    private static final Logger logger = LoggerFactory.getLogger(ChatController.class);
 
     @MessageMapping("/chat.sendMessage")
+    ///topic開頭的前綴才會經過前面config的設定 這裡設定public是代表訂閱了public的人都能收到這則訊息
     @SendTo("/topic/public")
+    //Payload用於websocket與STOMP請求資料
     public ChatMessage sendMessage(@Payload ChatMessage chatMessage) {
-        logger.info("Received message: {}", chatMessage.getContent());
-        logger.info("Sending message to all: {}", chatMessage);
         return chatMessage;
     }
 
@@ -25,10 +22,10 @@ public class ChatController {
     @SendTo("/topic/public")
     public ChatMessage addUser(@Payload ChatMessage chatMessage, 
                                SimpMessageHeaderAccessor headerAccessor) {
-        logger.info("User joined: {}", chatMessage.getSender());
-        // Add username in web socket session
+        // 將chatMessage.getSender()存進Session的username,這樣可以在Websocket中辨識這個用戶 在現有的websocket中儲存用戶信息
+        // 如果要使用websocket 結合JWT要建立WebSocketHandshakeInterceptor 在握手階段驗證token並將用戶attributes.put("username", username)
+        // 這樣就不用在這做session的Put
         headerAccessor.getSessionAttributes().put("username", chatMessage.getSender());
-        logger.info("Sending user joined message to all: {}", chatMessage);
         return chatMessage;
     }
 }
--- src/main/java/com/mason/chat/config/WebSocketConfig.java
+++ src/main/java/com/mason/chat/config/WebSocketConfig.java
@@ -12,12 +12,15 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
+        //註冊STOMP協議 會利用下面的/ws連線到websocket
         registry.addEndpoint("/ws").withSockJS();
     }
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
+        //註冊一個一個simpleBroker 負責將消息從發送者路由到一個或多個接收者。
         config.enableSimpleBroker("/topic");
+        //發送訊息給伺服器前要有的前綴
         config.setApplicationDestinationPrefixes("/app");
     }
 }
